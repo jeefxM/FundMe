a7d909b3e654358601eb32fe6a42810bcf1dec80
jeefxM <87044530+jeefxM@users.noreply.github.com>
2022-10-30 16:47:57 +0400

    Add files via upload

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,28 @@
+const { network, ethers } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const { chainId } = network.config.chainId
+
+    let ethUsdPriceFeedAddress
+
+    // = networkConfig[chainId]["ethUsdPriceFeed"]
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress],
+        log: true
+    })
+
+    log("----------------------------------------------------")
+}
+
+module.exports.tags = ["all", "fundme"]
